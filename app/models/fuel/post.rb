f870f9760009ff5d2eec863c205166c87a835e34
Ryan Francis <ryan.p.francis@gmail.com>
2015-02-16 11:43:49 -0600

    fixed scope

--- app/models/fuel/post.rb
+++ app/models/fuel/post.rb
@@ -18,11 +18,11 @@ module Fuel
     scope :recent, -> { order("created_at DESC") }
 
     def next
-      recent.where("created_at <= ? AND id != ?", created_at, id).first
+      self.class.recent.where("created_at <= ? AND id != ?", created_at, id).first
     end
 
     def previous
-      recent.where("created_at >= ? AND id != ?", created_at, id).last
+      self.class.recent.where("created_at >= ? AND id != ?", created_at, id).last
     end
 
     def should_generate_new_friendly_id?

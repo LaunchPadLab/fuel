6417d8f331804936ab676ff5706abbc019e51751
Ryan Francis <ryan.p.francis@gmail.com>
2015-02-16 23:54:02 -0600

    removed console.log

--- app/assets/javascripts/fuel/facebook.js
+++ app/assets/javascripts/fuel/facebook.js
@@ -3,7 +3,6 @@ $(function() {
   $(".fuel-fb").on('click', function(e){
     e.preventDefault();
     var url = $(this).data('url');
-    console.log(url);
     FB.ui({
       method: 'share',
       href: url,

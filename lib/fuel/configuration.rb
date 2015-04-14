bd9919586cc65ea1ab7d7170a7b508cd5333ff8f
Ryan Francis <ryan.p.francis@gmail.com>
2015-04-14 16:27:43 -0500

    begun to add authors and tags

--- lib/fuel/configuration.rb
+++ lib/fuel/configuration.rb
@@ -2,7 +2,7 @@ module Fuel
   class Configuration
 
     # What kind of comments do you want to add to your blog ? (:active_record, :disqus or :no)
-    attr_accessor :layout, :blog_title, :disqus_name, :twitter, :username, :password, :helpers, :paginates_per, :featured_image_settings, :facebook_app_id, :site_name
+    attr_accessor :layout, :blog_title, :disqus_name, :twitter, :username, :password, :helpers, :paginates_per, :featured_image_settings, :facebook_app_id, :site_name, :authors, :tags
 
     def initialize
       @layout = "application"
@@ -20,6 +20,8 @@ module Fuel
       @paginates_per = 5
       @facebook_app_id = nil
       @site_name = nil
+      @authors = ['John Doe', 'Jane Smith']
+      @tags = ['Tag 1', 'Tag 2']
     end
 
   end

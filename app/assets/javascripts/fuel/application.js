e42aad0b67ae75ceaa9539912facd22dd2006e06
Ryan Francis <ryan.p.francis@gmail.com>
2015-06-05 13:43:40 -0500

    only load rich text editor on form

--- app/assets/javascripts/fuel/application.js
+++ app/assets/javascripts/fuel/application.js
@@ -19,3 +19,4 @@
 //= require fuel/datepicker
 //= require fuel/posts
 //= require froala_editor.min.js
+//= require fuel/rich-text
---
+++ app/assets/javascripts/fuel/rich-text.js
@@ -0,0 +1,7 @@
+$(function() {
+  $body = $("body");
+
+  if ($("body").hasClass("posts-controller")){
+    $('#fuel_post_content').editable({inlineMode: false, theme: 'gray'})
+  };
+});

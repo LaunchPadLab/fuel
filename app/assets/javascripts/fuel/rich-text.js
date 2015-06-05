61d905e6e5a4e76062e45388d7327c34866024dc
Ryan Francis <ryan.p.francis@gmail.com>
2015-06-05 14:42:37 -0500

    quick tweaks

--- app/assets/javascripts/fuel/rich-text.js
+++ app/assets/javascripts/fuel/rich-text.js
@@ -2,6 +2,8 @@ $(function() {
   $body = $("body");
 
   if ($("body").hasClass("posts-controller")){
-    $('#fuel_post_content').editable({inlineMode: false, theme: 'gray'})
+    $('#fuel_post_content').editable({
+      inlineMode: false, theme: 'gray', height: '500'
+    })
   };
 });

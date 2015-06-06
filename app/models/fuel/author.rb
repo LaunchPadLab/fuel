300d1d1d683766c712a4070a48990fc2e461adb8
kurtcunningham <cunningham.kurt@gmail.com>
2015-06-06 11:10:04 -0500

    various small design updates

--- app/models/fuel/author.rb
+++ app/models/fuel/author.rb
@@ -11,7 +11,7 @@ module Fuel
       [first_name, last_name].compact.join(" ")
     end
 
-    has_attached_file :avatar, :styles => { :medium => Fuel.configuration.avatar_settings[:styles][:medium], :thumb => Fuel.configuration.avatar_settings[:styles][:thumb] }, :default_url => "/images/:style/missing.png"
+    has_attached_file :avatar, :styles => { :medium => Fuel.configuration.avatar_settings[:styles][:medium], :thumb => Fuel.configuration.avatar_settings[:styles][:thumb] }, :default_url => "fuel/default-img.jpg"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   end
